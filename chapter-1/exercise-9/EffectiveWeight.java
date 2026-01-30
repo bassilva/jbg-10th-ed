a6a115df9a3e76d7ca6ada09f187c3ab2e39f113
Bruno Almeida da Silva <eng.fis.bruno@gmail.com>
2026-01-30 12:46:57 -0300

    Fix chapter-1/exercise-9

--- chapter-1/exercise-9/EffectiveWeight.java
+++ chapter-1/exercise-9/EffectiveWeight.java
@@ -1,11 +1,9 @@
 class EffectiveWeight {
 	public static void main(String[] args) {
 		double earthGravity = 9.81; // m / s**2
-		double moonGravity = 0.17 * earthGravity;
 		double myMass = 48.0; // Kilograms
-		double myEarthWeight = myMass * earthGravity;
 
-		double myMoonWeight = 0.17 * myMass;
+		double myMoonWeight = 0.17 * myMass * earthGravity;
 
 		System.out.println("My Weight on the Moon is: " +
 				myMoonWeight + " N");
