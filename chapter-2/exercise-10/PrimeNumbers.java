2383e4e17f44c6b301ea64ef034c72b9f7707747
Bruno Almeida da Silva <eng.fis.bruno@gmail.com>
2026-02-19 07:26:47 -0300

    Add Chapter 2 Exercise 10

---
+++ chapter-2/exercise-10/PrimeNumbers.java
@@ -0,0 +1,16 @@
+class PrimeNumbers {
+	public static void main(String[] args) {
+		for(int i = 2; i <= 100; i++) {
+			int count = 0;
+			for(int j = i - 1; j > 0; j--) {
+				if(i % j == 0) {
+					count++;
+				}
+			}
+
+			if(count == 1) {
+				System.out.println(i + " is a prime number.");
+			}
+		}
+	}
+}
